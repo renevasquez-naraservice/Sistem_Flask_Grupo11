dd75dbaafd7e7be9838c5a3771cfabfd41393ee3
DaniUAB <daniel.mollinedo@uab.edu.bo>
2026-03-08 16:49:25 -0400

    Registro del blueprint de categorias y carga del modelo categoria

--- app/__init__.py
+++ app/__init__.py
@@ -18,13 +18,16 @@ def create_app(config_class=Config):
     
     # Importar modelos (para migraciones)
     from .models import user
+    from .models import categoria
     
     from .routes.auth import auth_bp                    # De auth.py
     from .routes.admin import admin_dashboard_bp        # De admin.py 
+    from .routes.categorias import categorias_bp        # De categoria.py
     
     # Registrar blueprints
     app.register_blueprint(auth_bp)
     app.register_blueprint(admin_dashboard_bp)          # Registrar el blueprint admin
+    app.register_blueprint(categorias_bp)               # Registrar el blueprint categoria
     
     return app
 
