0318a19ad3fd06d69dda0dc9648725ff9990d479
DaniUAB <daniel.mollinedo@uab.edu.bo>
2026-03-08 16:30:52 -0400

    Se crea el modelo Categoria

---
+++ app/models/categoria.py
@@ -0,0 +1,11 @@
+from ..extensions import db
+
+class Categoria(db.Model):
+    __tablename__ = "categorias"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False, unique=True)
+    descripcion = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"<Categoria {self.nombre}>"
