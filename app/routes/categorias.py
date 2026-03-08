2e07e1bccd5d58db8ad2f88bdd0fc4ec2e2000b7
DaniUAB <daniel.mollinedo@uab.edu.bo>
2026-03-08 17:06:49 -0400

    feat: crear categorias desde formulario web

--- app/routes/categorias.py
+++ app/routes/categorias.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from ..models.categoria import Categoria
 from ..extensions import db
 
@@ -14,8 +14,23 @@ def lista():
     return render_template("categorias/lista.html", categorias=categorias)
 
 
-@categorias_bp.route("/crear")
+@categorias_bp.route("/crear", methods=["GET", "POST"])
 def crear():
+
+    if request.method == "POST":
+        nombre = request.form["nombre"]
+        descripcion = request.form["descripcion"]
+
+        nueva_categoria = Categoria(
+            nombre=nombre,
+            descripcion=descripcion
+        )
+
+        db.session.add(nueva_categoria)
+        db.session.commit()
+
+        return redirect(url_for("categorias.lista"))
+
     return render_template("categorias/crear.html")
 
 
